b43e69c175ec7943071440fdfbac4ba656045ec8
Mohamed Kaba <muhamad.kaba@gmail.com>
2019-01-14 12:01:44 +0100

    first commit et solution

---
+++ src/main/java/com/wildcodeschool/springHibernateQuest/SpringHibernateQuestApplication.java
@@ -0,0 +1,14 @@
+package com.wildcodeschool.springHibernateQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateQuestApplication.class, args);
+	}
+
+}
+
